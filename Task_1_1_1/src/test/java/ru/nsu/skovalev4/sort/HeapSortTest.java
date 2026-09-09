851912c19888fd2ce3003b4ed987661f4803bab3
Stanislav Kovalev <kovstas2007@mail.ru>
2026-09-09 10:44:25 +0700

    fix last reviewdog warning

--- Task_1_1_1/src/test/java/ru/nsu/skovalev4/sort/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/skovalev4/sort/HeapSortTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.skovalev4.sort;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
 
 class HeapSortTest {
 
