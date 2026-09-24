6967bea23a3334fe108d03d74aeaa0a193558688
Stanislav Kovalev <kovstas2007@mail.ru>
2026-09-24 13:12:50 +0700

    add Deck Test

---
+++ Task_1_2_1/src/test/java/ru/nsu/skovalev4/blackjack/DeckTest.java
@@ -0,0 +1,42 @@
+package ru.nsu.skovalev4.blackjack;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
+
+
+class DeckTest {
+
+    @Test
+    void deckContainsFiftyTwoUniqueCards() {
+        Deck deck = new Deck();
+        deck.shuffleDeck();
+        Set<String> cards = new HashSet<>();
+        for (int i = 0; i < 52; i++) {
+            Card card = deck.getCard();
+            String key = card.getSuit() + ":" + card.getRank();
+            assertTrue(cards.add(key));
+        }
+        assertEquals(52, cards.size());
+    }
+
+    @Test
+    void resetRestoresFullDeck() {
+        Deck deck = new Deck();
+        deck.getCard();
+        deck.getCard();
+        deck.getCard();
+        deck.reset();
+        Set<String> cards = new HashSet<>();
+        for (int i = 0; i < 52; i++) {
+            Card card = deck.getCard();
+            String key = card.getSuit() + ":" + card.getRank();
+            assertTrue(cards.add(key));
+        }
+        assertEquals(52, cards.size());
+    }
+}
